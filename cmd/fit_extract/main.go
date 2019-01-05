1651e0e6e5a52af9db2de9ebf19f6183dbd1aa99
Dominic Green <dominicgreen1@gmail.com>
2019-01-05 13:53:57 +0000

    noddy error handling

--- cmd/fit_extract/main.go
+++ cmd/fit_extract/main.go
@@ -71,7 +71,11 @@ func main() {
 			req, _ = http.NewRequest("GET", v.HeartRateLink, nil)
 			req.Header.Add("Authorization", "Bearer "+*ft)
 
-			resp, _ = http.DefaultClient.Do(req)
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
 			body, _ = ioutil.ReadAll(resp.Body)
 			defer resp.Body.Close()
 			var a = new(fitbit.ActivityFullHeartRate)
